f4cc52fecb386ab7c0a59dc415c6432c7d5c2025
msaichaitanya735 <43997513+msaichaitanya735@users.noreply.github.com>
2022-02-17 01:40:57 +0530

    MyPortfolio | Homepage with mobile responsiveness

---
+++ src/components/HomePage.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Homepage.css'
+import me from '../images/chay2.png'
+
+const HomePage=()=> {
+  return (
+    <div className="container">
+      <div className="logo">
+        Chay
+      </div>
+      <div className="projects">
+        Projects
+      </div>
+      <div className="about">
+        About
+      </div>
+      <div className="myskills">
+        My Skills
+      </div>
+      <div className="contact">
+        Contact
+      </div>
+      <div className="centerbox">
+        <div className="info">
+          <span className='hi' >Hi, </span>
+          <span className='chaitanya'>I'm Chaitanya</span>
+          <span className='details' >I am a Frontend Developer. I code and design web pages.</span>
+        </div> 
+        <div className="imgcontainer">
+          <div className="img">
+            <img src={me} alt="Chaitanya" />
+          </div> 
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
