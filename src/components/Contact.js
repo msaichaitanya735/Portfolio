2bd993d6cfd17647b3f236e126aea31811b62256
msaichaitanya735 <43997513+msaichaitanya735@users.noreply.github.com>
2022-02-20 03:55:33 +0530

    projects and contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,14 +7,14 @@ const Contact = () => {
     <div className="contactcontainer">
         <div className="logo"><a href="/" style={{color:'black'}}>Chay</a></div>
         <div className="contacts">&#60;CONTACT /&#62;</div>
-        <div className="projimgcontainer">
+        <div className="contactimgcontainer">
                 <div className="img">
                     <img src={me} alt="chaitanya"/>
                 </div>            
         </div>
         <div className="discuss">Let's Discuss</div>
         <div className="contacts">
-          
+
         </div>
     </div>    
   )
